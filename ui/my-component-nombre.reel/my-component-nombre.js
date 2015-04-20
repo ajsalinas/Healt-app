ab9df54def4893bcad98c07f7bf8a75d208af33e
ajsalinas <noreply>
2015-04-20 23:55:21 +0000

    Add component my-component-nombre

---
+++ ui/my-component-nombre.reel/my-component-nombre.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/my-component-nombre.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MyComponentNombre
+ * @extends Component
+ */
+exports.MyComponentNombre = Component.specialize(/** @lends MyComponentNombre# */ {
+    constructor: {
+        value: function MyComponentNombre() {
+            this.super();
+        }
+    }
+});
